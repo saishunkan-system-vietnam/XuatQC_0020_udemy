f92d52ffce0337da8b9b2739e912e5ad5b6f22de
XuatQC <xuatqc@saishunkansys.com>
2023-12-03 17:58:59 +0700

    Added Assignment prj

---
+++ Asp.Net Core 7 (.NET 7)  True Ultimate/Demo/16.CRUD/Assigment/CRUDExampleSolution/ServiceContracts/DTO/TradeOrderResponse.cs
@@ -0,0 +1,61 @@
+﻿using Entities;
+using ServiceContracts.Enums;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// DTO class return type for TradeOrderResponse(both of buy and sell orders) most of Finhubservice methods
+    /// </summary>
+    public class TradeOrderResponse
+    {
+        public Guid OrderID { get; set; }
+
+        public string StockSymbol { get; set; } = string.Empty;
+
+        public string StockName { get; set; } = string.Empty;
+
+        public DateTime DateAndTimeOfOrder { get; set; }
+
+        public uint Quantity { get; set; }
+
+        public double Price { get; set; }
+
+        public double TradeAmount { get; set; }
+
+        public string OrderType { get; set; } = string.Empty;
+
+    }
+
+    public static class TradeOrderResponseExtension
+    {
+        public static TradeOrderResponse ToTradeOrderResponse(this SellOrder sellOrder)
+        {
+            return new TradeOrderResponse()
+            {
+                OrderID = sellOrder.SellOrderID,
+                StockSymbol = sellOrder.StockSymbol,
+                StockName = sellOrder.StockName,
+                DateAndTimeOfOrder = sellOrder.DateAndTimeOfOrder,
+                Quantity = sellOrder.Quantity,
+                Price = sellOrder.Price,
+                OrderType = OrderTypeOptions.Sell.ToString(),
+                TradeAmount = sellOrder.Price * sellOrder.Quantity
+            };
+        }
+
+        public static TradeOrderResponse ToTradeOrderResponse(this BuyOrder buyOrder)
+        {
+            return new TradeOrderResponse()
+            {
+                OrderID = buyOrder.BuyOrderID,
+                StockSymbol = buyOrder.StockSymbol,
+                StockName = buyOrder.StockName,
+                DateAndTimeOfOrder = buyOrder.DateAndTimeOfOrder,
+                Quantity = buyOrder.Quantity,
+                Price = buyOrder.Price,
+                OrderType = OrderTypeOptions.Buy.ToString(),
+                TradeAmount = buyOrder.Price * buyOrder.Quantity
+            };
+        }
+    }
+}
---
+++ Asp.Net Core 7 (.NET 7)  True Ultimate/Demo/16.CRUD/Assigment/CRUDExampleSolution/Services/StocksService.cs
@@ -0,0 +1,135 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using Services.Helper;
+
+namespace Services
+{
+    public class StocksService : IStocksService
+    {
+
+        private readonly TradeOrderDBContext _dbContext;
+        public StocksService(TradeOrderDBContext tradeOrderDBContext) 
+        {
+            _dbContext = tradeOrderDBContext;
+
+        }
+        /// <summary>
+        /// Create buy order
+        /// </summary>
+        /// <param name="buyOrderRequest">buy order request</param>
+        /// <returns>Buy order response</returns>
+        public async Task<BuyOrderResponse> CreateBuyOrder(BuyOrderRequest? buyOrderRequest)
+        {
+
+            if (buyOrderRequest == null)
+            {
+                throw new ArgumentNullException(nameof(buyOrderRequest));
+            }
+
+            ValidationHelper.ModelValidation(buyOrderRequest);
+
+            DateTime minDate = Convert.ToDateTime("2000-01-01");
+
+            if (buyOrderRequest.DateAndTimeOfOrder < minDate)
+            {
+                throw new ArgumentException("Order datetime should be equal or newer date than 2000-01-01");
+            }
+
+            // Save buy order
+            BuyOrder buyOrder = buyOrderRequest.ToBuyOrder();
+            buyOrder.BuyOrderID = Guid.NewGuid();
+
+            _dbContext.BuyOrders.Add(buyOrder);
+            _dbContext.SaveChanges();
+
+            // return buy order response
+            BuyOrderResponse response = buyOrder.ToBuyOrderResponse();
+
+            return response;
+        }
+
+        /// <summary>
+        /// Create sell order
+        /// </summary>
+        /// <param name="sellOrderRequest">sell order request</param>
+        /// <returns>Sell order response</returns>
+        public async Task<SellOrderResponse> CreateSellOrder(SellOrderRequest? sellOrderRequest)
+        {
+            if (sellOrderRequest == null)
+            {
+                throw new ArgumentNullException(nameof(sellOrderRequest));
+            }
+
+            ValidationHelper.ModelValidation(sellOrderRequest);
+
+            DateTime minDate = Convert.ToDateTime("2000-01-01");
+
+            if (sellOrderRequest.DateAndTimeOfOrder < minDate)
+            {
+                throw new ArgumentException("Order datetime should be equal or newer date than 2000-01-01");
+            }
+
+            // Save sell order
+            SellOrder sellOrder = sellOrderRequest.ToSellOrder();
+            sellOrder.SellOrderID = Guid.NewGuid();
+
+            _dbContext.SellOrders.Add(sellOrder);
+            _dbContext.SaveChanges();
+
+            // return sell order response
+            SellOrderResponse response = sellOrder.ToSellOrderResponse();
+
+            return response;
+        }
+
+        /// <summary>
+        /// Get list of buy order
+        /// </summary>
+        /// <returns>Return buy order response as list of buy order</returns>
+        public async Task<List<BuyOrderResponse>> GetBuyOrders()
+        {
+            List <BuyOrderResponse> buyOrdersResponse = new List<BuyOrderResponse> ();
+            foreach (var buyOrder in _dbContext.BuyOrders.ToList())
+            {
+                buyOrdersResponse.Add(buyOrder.ToBuyOrderResponse());
+            }
+            return buyOrdersResponse;
+        }
+
+        /// <summary>
+        /// Get list of sell order
+        /// </summary>
+        /// <returns>Return sell order response as list of sell order</returns>
+        public  async Task<List<SellOrderResponse>> GetSellOrders()
+        {
+            List<SellOrderResponse> sellOrdersResponse = new List<SellOrderResponse>();
+            foreach (var sellOrder in _dbContext.SellOrders.ToList())
+            {
+                sellOrdersResponse.Add(sellOrder.ToSellOrderResponse());
+            }
+            return sellOrdersResponse;
+        }
+
+        /// <summary>
+        /// Get list of both buy and sell order
+        /// </summary>
+        /// <returns>Return tradeOrders response that present for all data of both to table are BuyOrder and SellOrder</returns>
+        public async Task<List<TradeOrderResponse>> GetTradeOrders()
+        {
+            List<TradeOrderResponse> tradeOrdersResponse = new List<TradeOrderResponse>();
+
+            foreach (BuyOrder buyOrder in _dbContext.BuyOrders.ToList())
+            {
+                tradeOrdersResponse.Add(buyOrder.ToTradeOrderResponse());
+            }
+
+            foreach (var sellOrder in _dbContext.SellOrders.ToList())
+            {
+                tradeOrdersResponse.Add(sellOrder.ToTradeOrderResponse());
+            }
+
+            return tradeOrdersResponse;
+        }
+    }
+}
