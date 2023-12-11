016c12c62816732177ae65b2d615bb9897c12c91
XuatQC <xuatqc@saishunkansys.com>
2023-12-11 14:52:04 +0700

    commit for added Remote validation project

---
+++ Asp.Net Core 7 (.NET 7)  True Ultimate/Demo/07.Validation/RemoteValidation/RemoteValidation/Models/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RemoteValidation.Models
+{
+    public class UserViewModel
+    {
+        [Remote(action: "IsUsernameValid", controller: "User")] // remote attribute handle validation by specigy action(validation action method) and controller to
+        public string Username { get; set; }
+    }
+}
---
+++ Asp.Net Core 7 (.NET 7)  True Ultimate/Demo/07.Validation/RemoteValidation/ServiceContracts/DTO/CountryResponse.cs
@@ -0,0 +1,33 @@
+﻿using Entities;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// DTO class return type for most of countriesService methods
+    /// </summary>
+    public class CountryResponse
+    {
+        public Guid CountryID { get; set; }
+        public string? CountryName { get; set; }
+
+        public override bool Equals(object? country2Compare)
+        {
+            if (country2Compare == null) return false;
+
+            if (country2Compare.GetType() != typeof(CountryResponse)) return false;
+
+            CountryResponse countryParam = (CountryResponse)country2Compare;
+
+            return this.CountryID == countryParam.CountryID &&
+                this.CountryName == countryParam.CountryName;
+        }
+    }
+
+    public static class CountyExtension
+    {
+        public static CountryResponse ToCountryRespone(this Country country)
+        {
+            return new CountryResponse { CountryID = country.ID, CountryName = country.Name };
+        }
+    }
+}
