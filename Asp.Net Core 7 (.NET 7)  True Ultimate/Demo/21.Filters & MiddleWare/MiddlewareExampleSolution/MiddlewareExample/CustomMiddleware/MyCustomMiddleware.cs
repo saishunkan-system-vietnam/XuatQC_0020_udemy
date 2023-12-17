02b788b8c2e7d276b6e8f1c51f4008027aca93f5
XuatQC <xuatqc@saishunkansys.com>
2023-12-17 23:18:33 +0700

    Middleware

---
+++ Asp.Net Core 7 (.NET 7)  True Ultimate/Demo/21.Filters & MiddleWare/MiddlewareExampleSolution/MiddlewareExample/CustomMiddleware/MyCustomMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace MiddlewareExample.CustomMiddleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class MyCustomMiddleware: IMiddleware
+    {
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("My custom middleware start");
+            await next(context);
+            await context.Response.WriteAsync("My custom middleware end");
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMyCustomMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MyCustomMiddleware>();
+        }
+    }
+}
---
+++ Asp.Net Core 7 (.NET 7)  True Ultimate/Demo/21.Filters & MiddleWare/MiddlewareExampleSolution/MiddlewareExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using MiddlewareExample.CustomMiddleware;
+using System.Linq;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<MyCustomMiddleware>();
+var app = builder.Build();
+
+
+// Middleware 1
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Hello world\n");
+    await next(context);
+});
+
+// Middleware 2
+//app.Use(async (HttpContext context, RequestDelegate next) =>
+//{
+//    await context.Response.WriteAsync("Hello again");
+//    await next(context);
+//});
+
+//app.UseMyCustomMiddleware();
+
+// Middleware conventional với điều kiện 
+// app.UseHelloWorldCustomMiddleware();
+
+// run middle if condition is true
+app.UseWhen(context => context.Request.Query.ContainsKey("userName"), app =>
+{
+    app.Use(async (context, next) =>
+    {
+        await context.Response.WriteAsync("Hello world from middleware branch\n");
+        await next(context);
+    });
+});
+
+
+// Middleware 3
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("More Hello again\n");
+});
+app.Run();
