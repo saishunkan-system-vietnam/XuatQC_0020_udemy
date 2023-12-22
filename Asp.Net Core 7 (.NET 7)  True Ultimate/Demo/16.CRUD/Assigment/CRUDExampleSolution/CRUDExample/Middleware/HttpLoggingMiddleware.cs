acc61a6a8bef768c6a82b9bdcaf5e4663b4a8e48
XuatQC <xuatqc@saishunkansys.com>
2023-12-22 15:25:41 +0700

    addded file and rename

---
+++ Asp.Net Core 7 (.NET 7)  True Ultimate/Demo/16.CRUD/Assigment/CRUDExampleSolution/CRUDExample/Middleware/HttpLoggingMiddleware.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Microsoft.IO;
+using System.Net.Http;
+
+namespace CRUDExample.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class HttpLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly RecyclableMemoryStreamManager recyclableMemoryStreamManager;
+        private readonly ILogger<HttpLoggingMiddleware> _logger;
+
+
+        public HttpLoggingMiddleware(RequestDelegate next, ILogger<HttpLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+            recyclableMemoryStreamManager = new RecyclableMemoryStreamManager();
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await LogRequest(context);
+            await LogResponse(context);
+        }
+
+        private async Task LogRequest(HttpContext context)
+        {
+            context.Request.EnableBuffering();
+
+            await using var requestStream = recyclableMemoryStreamManager.GetStream();
+            await context.Request.Body.CopyToAsync(requestStream);
+
+            _logger.LogInformation($"REQUEST Http Request Information:{Environment.NewLine}" +
+                                   $"Schema:{context.Request.Scheme} " +
+                                   $"Host: {context.Request.Host} " +
+                                   $"Path: {context.Request.Path} " +
+                                   $"QueryString: {context.Request.QueryString} " +
+                                   $"Request Body: {ReadStreamInChunks(requestStream)}");
+            context.Request.Body.Position = 0;
+        }
+
+        private async Task LogResponse(HttpContext context)
+        {
+            var originalBodyStream = context.Response.Body;
+
+            await using var responseBody = recyclableMemoryStreamManager.GetStream();
+            context.Response.Body = responseBody;
+
+            await _next(context);
+
+            context.Response.Body.Seek(0, SeekOrigin.Begin);
+            var text = await new StreamReader(context.Response.Body).ReadToEndAsync();
+            context.Response.Body.Seek(0, SeekOrigin.Begin);
+
+            _logger.LogInformation($"RESPONSE Http Response Information:{Environment.NewLine}" +
+                                   $"Schema:{context.Request.Scheme} " +
+                                   $"Host: {context.Request.Host} " +
+                                   $"Path: {context.Request.Path} " +
+                                   $"QueryString: {context.Request.QueryString} " +
+                                   $"Response Body: {text}");
+
+            await responseBody.CopyToAsync(originalBodyStream);
+        }
+
+        private static string ReadStreamInChunks(Stream stream)
+        {
+            const int readChunkBufferLength = 4096;
+
+            stream.Seek(0, SeekOrigin.Begin);
+
+            using var textWriter = new StringWriter();
+            using var reader = new StreamReader(stream);
+
+            var readChunk = new char[readChunkBufferLength];
+            int readChunkLength;
+
+            do
+            {
+                readChunkLength = reader.ReadBlock(readChunk, 0, readChunkBufferLength);
+                textWriter.Write(readChunk, 0, readChunkLength);
+            } while (readChunkLength > 0);
+
+            return textWriter.ToString();
+        }
+
+    
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class HttpLoggingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseHttpLoggingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<HttpLoggingMiddleware>();
+        }
+    }
+}
